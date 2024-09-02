596c478bda52109c26007823b3557ea357f9d527
Adam-Elmi <Adamcade123@gmail.com>
2024-09-03 02:46:11 +0300

    Add copyClear and copyHistory methods to TypeScript definitions

--- copy-share.d.ts
+++ copy-share.d.ts
@@ -2,7 +2,10 @@
 declare module 'copy-share' {
     export function copyText(text: string): void;
     export function copyImage(imageUrl: string): void;
-    export function copyVideoUrl(imageUrl: string): void;
+    export function copyVideoUrl(videoUrl: string): void;
     export function copyCode(code: string): void;
     export function copyLink(url: string): void;
+    export function copyClear(): void;
+    export function copyHistory(): string[];
 }
+
