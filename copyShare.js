992d0e08f47f30d8632f37bfaa7aa2243ce5a108
Adam-Elmi <Adamcade123@gmail.com>
2024-09-03 03:43:46 +0300

    Fix font size issue and update notifyElement styles

--- copyShare.js
+++ copyShare.js
@@ -326,7 +326,7 @@ class CopyShare {
             text-align: center;
             line-height: 2;
             color: white;
-            font-size: 1px; 
+            font-size: 16px; 
         `;
 
         div.appendChild(notifyElement);
